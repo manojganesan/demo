2497984adaf4009d018b86d330e00889a833dbf8
MANOJ G <manoj3493@gmail.com>
2020-10-03 23:20:58 +0800

    inital commit

---
+++ src/main/java/config/BeanConfig.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.saml.provider.SamlServerConfiguration;
+import org.springframework.security.saml.provider.service.config.SamlServiceProviderServerBeanConfiguration;
+
+@Configuration
+public class BeanConfig extends SamlServiceProviderServerBeanConfiguration {
+
+	private final AppConfig config;
+
+	public BeanConfig(AppConfig config) {
+		this.config = config;
+	}
+
+	@Override
+	protected SamlServerConfiguration getDefaultHostSamlServerConfiguration() {
+		return config;
+	}
+}
